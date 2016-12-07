5727e5b63c5ba53d2c72f36f55fb93ccefcb8908
Alan Andrade <alan.andradec@gmail.com>
2016-12-06 20:51:42 -0800

    Playlists are sortable

--- popup/playlist.js
+++ popup/playlist.js
@@ -1,53 +1,93 @@
 var storage = browser.storage.local;
 
-storage.get(["playlist", "currentTrack"]).then(function (res) {
-    var playlist = res["playlist"]
-        , currentTrack = res["currentTrack"];
+const CurrentTrack  = 'currentTrack';
+const Playlist      = 'playlist';
 
-    var app = new Vue({
-        el: '#app',
-        data: {
-            playlist: playlist,
-            currentTrack: currentTrack
-        },
-        methods: {
-            playTrack: function(track) {
-                console.log('current url:' + app.currentTrack.url);
+function setCurrentTrack(track) {
+    storage.set({[CurrentTrack]: track});
+}
 
-                function openTab(track) {
-                    browser.tabs.create({
-                        url: track.url,
-                        active: false,
-                        index: 0
-                    });
-                }
+function setPlaylist(playlist) {
+    storage.set({[Playlist]: playlist});
+}
 
-                if (app.currentTrack) {
-                    browser.tabs.query({
-                        url: app.currentTrack.url
-                    }).then(function(tabs) {
-                        storage.set({'currentTrack': track});
-                        app.currentTrack = track;
+function getCurrentTrack(queryResult) {
+    return queryResult[CurrentTrack];
+}
 
-                        if (!tabs.length) {
-                            openTab(track);
-                            return
-                        }
-                        browser.tabs.update(tabs[0].id, {
-                            url: track.url
-                        });
-                    });
-                } else {
-                    openTab(track);
-                    storage.set({'currentTrack': track});
-                    app.currentTrack = track;
-                }
-            },
+function getPlaylist(queryResult) {
+    return queryResult[Playlist];
+}
 
-            clearPlaylist: function() {
-                storage.clear();
-                app.playlist = [];
-            }
+var App = new Vue({
+    el: '#app',
+    data: {
+        playlist: [],
+        currentTrack: null
+    },
+    watch: {
+        playlist: function(newValue) {
+            setPlaylist(newValue);
         }
-    });
+    },
+    methods: {
+        playTrack: function(track) {
+            function openTab(track) {
+                browser.tabs.create({
+                    url: track.url,
+                    active: false,
+                    index: 0
+                });
+            }
+
+            if (this.currentTrack) {
+                browser.tabs.query({
+                    url: this.currentTrack.url
+                }).then(function(tabs) {
+                    this.currentTrack = track;
+
+                    if (!tabs.length) {
+                        openTab(track);
+                        return
+                    }
+                    browser.tabs.update(tabs[0].id, {
+                        url: track.url
+                    });
+                });
+            } else {
+                openTab(track);
+                this.currentTrack = track;
+            }
+        },
+
+        clearPlaylist: function() {
+            storage.clear();
+            this.playlist = [];
+        },
+
+        removeTrack: function(track) {
+            var trackIndex = this.playlist.findIndex(function(t) {
+                return t.url == track.url;
+            });
+            this.playlist.splice(trackIndex, 1);
+        },
+
+        onSortableUpdate: function(event) {
+            // Hack to make it work with Vue 2
+            var clonedItems = this.playlist.filter(function(item){
+                return item;
+            });
+            clonedItems.splice(event.newIndex, 0, clonedItems.splice(event.oldIndex, 1)[0]);
+            var self = this;
+            self.playlist = [];
+            Vue.nextTick(function(){
+                self.playlist = clonedItems;
+            });
+        }
+    }
+});
+
+storage.get([Playlist, CurrentTrack]).then(function (res) {
+    App.playlist = getPlaylist(res);
+    App.currentTrack = getCurrentTrack(res);
 });
